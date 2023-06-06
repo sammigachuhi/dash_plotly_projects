2f840c58503e7e4084fe4e764353165e702a8b76
Samuel Gachuhi <sammigachuhi@gmail.com>
2023-06-06 15:39:12 +0300

    Updated human_sanitation.py

--- human_sanitation.py
+++ human_sanitation.py
@@ -5,10 +5,9 @@ import pandas as pd
 df = pd.read_csv('sanitation_long2.csv')
 
 app = Dash(__name__)
-server = app.server
 
 app.layout = html.Div([
-    html.Div("App that shows Percentage Distribution of Human Waste Disposal Across Counties"),
+    html.Div("App that shows Percentage Distribution of Human Waste Disposal Across Counties (2019 Census)"),
 
     html.Hr(),
 
@@ -35,7 +34,7 @@ def update_figure(selected_county):
 
     fig = px.bar(filtered_county, x='indicator',
                  y='value', color='indicator',
-                 title="Percentage Distribution of Conventional Households by Main Mode of Human Waste Disposal",
+                 title="Percentage Distribution of Conventional Households by Main Mode of Human Waste Disposal According to Kenya 2019 Census",
                  height=600)
 
     return fig
