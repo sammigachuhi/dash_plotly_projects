40bc229f0e71dbcdd129156d0af635780e45d1a5
Samuel Gachuhi <sammigachuhi@gmail.com>
2023-06-28 17:15:11 +0300

    Almost 100% complete

--- intern_sanitation.py
+++ intern_sanitation.py
@@ -23,7 +23,13 @@ app.layout = html.Div([
 
     html.Hr(),
     # Indicate reason for zigzag lines
-    html.H6("Some indicators could have duplicate values, thus the reason for some line graphs appearing in a zigzag format"),
+    # html.H6("Some indicators could have duplicate values, thus the reason for some line graphs appearing in a zigzag format" + "<br>" +
+    #         "especially for `Total` and `Urban` variables",),
+    dcc.Markdown('''
+    *Some indicators could have duplicate values, thus the reason for some line graphs appearing in a zigzag format* 
+    
+    *This is especially for `Total` and `Urban` variables*
+    '''),
 
     html.Hr(),
 
@@ -50,6 +56,7 @@ app.layout = html.Div([
     html.Br(),
     html.Br(),
 
+    # Drinking water
     html.Div(className="row", children=[
         html.Div(className="six columns", children=[
             dcc.Graph(id="stack_bar_drinking_water", style={"display": "inline-block"})
@@ -59,49 +66,56 @@ app.layout = html.Div([
         ]),
     ]),
 
-    # html.Div(className="row", children=[
-    #     html.Div(className="six columns", children=[
-    #         dcc.Graph("stack_bar_sanitation_services")
-    #     ]),
-    #     html.Div(className="six columns", children=[
-    #         dcc.Graph("line_graph_sanitation_services")
-    #     ])
-    # ]),
+    # Sanitation services
+    html.Div(className="row", children=[
+        html.Div(className="six columns", children=[
+            dcc.Graph("stack_bar_sanitation_services")
+        ]),
+        html.Div(className="six columns", children=[
+            dcc.Graph("line_graph_sanitation_services")
+        ])
+    ]),
 
-    # html.Div(className="row", children=[
-    #     html.Div(className="six columns", children=[
-    #         dcc.Graph("stack_bar_handwashing")
-    #     ]),
-    #     html.Div(className="six columns", children=[
-    #         dcc.Graph("line_graph_handwashing")
-    #     ])
-    # ]),
+    # Handwashing
+    html.Div(className="row", children=[
+        html.Div(className="six columns", children=[
+            dcc.Graph("stack_bar_handwashing")
+        ]),
+        html.Div(className="six columns", children=[
+            dcc.Graph("line_graph_handwashing")
+        ])
+    ]),
 
-    # html.Div(className="row", children=[
-    #     html.Div(className="six columns", children=[
-    #         dcc.Graph("stack_bar_defecation")
-    #     ]),
-    #     html.Div(className="six columns", children=[
-    #         dcc.Graph("line_graph_defecation")
-    #     ])
-    # ])
+    # Open defecation
+    html.Div(className="row", children=[
+        html.Div(className="six columns", children=[
+            dcc.Graph("stack_bar_defecation")
+        ]),
+        html.Div(className="six columns", children=[
+            dcc.Graph("line_graph_defecation")
+        ])
+    ])
 ])
 
+# Bar chart for drinking water
 @app.callback(
     Output("stack_bar_drinking_water", "figure"),
     Input("country_dropdown", "value"),
     Input("residence_dropdown", "value")
 )
 def update_stack_drinking(x_axis_column_name, residence_type):
-    dff = df_drinking[df_drinking.Country.isin(x_axis_column_name)]
+    dff = df_drinking[df_drinking['Country'].isin(x_axis_column_name)]
 
     fig = px.histogram(dff[dff['Residence Area Type'] == residence_type], x='Country', y='Display Value', histfunc='avg',
-                       color='Country', title="Bar Chart Showing Population using safely \n"
-                                              " managed drinking-water services (%)",
+                       color='Country', title="Bar Chart Showing Average Percentage (%) of Population using safely " + "<br>" +
+                                              "managed drinking-water services (%)",
                        color_discrete_map=c)
+    fig.update_layout(transition={'duration': 100,
+                                  'easing': 'linear'})
 
     return fig
 
+# Line chart for drinking
 @app.callback(
     Output("line_graph_drinking_water", "figure"),
     Input("country_dropdown", "value"),
@@ -113,9 +127,127 @@ def update_line_drinking(x_axis_column_name, residence_type):
     dff = dff.sort_values(by='Year')
 
     fig = px.line(dff[dff['Residence Area Type'] == residence_type], x='Year', y='Display Value', markers=True,
-                      color='Country', title="Line Chart Showing Population using safely \n"
+                      color='Country', title="Line Chart Showing Population using safely " + "<br>" +
                                              "managed drinking-water services (%)",
                   color_discrete_map=c)
+    fig.update_layout(transition={'duration': 100,
+                                  'easing': 'linear'})
+
+    return fig
+
+# Bar chart for sanitation services
+@app.callback(
+    Output("stack_bar_sanitation_services", "figure"),
+    Input("country_dropdown", "value"),
+    Input("residence_dropdown", "value")
+)
+def update_stack_sanitation(x_axis_column_name, residence_type):
+    dff = df_sanitation[df_sanitation['Country'].isin(x_axis_column_name)]
+
+    fig = px.histogram(dff[dff['Residence Area Type'] == residence_type], x='Country', y='Display Value',
+                       histfunc='avg',
+                       color='Country', title="Bar Chart Showing Average Percentage (%) of Population using safely " + "<br>" +
+                                              "managed sanitation services",
+                       color_discrete_map=c)
+    fig.update_layout(transition={'duration': 100,
+                                  'easing': 'linear'})
+
+    return fig
+
+# Line Graph for sanitation services
+@app.callback(
+    Output("line_graph_sanitation_services", "figure"),
+    Input("country_dropdown", "value"),
+    Input("residence_dropdown", "value")
+)
+def update_line_sanitation(x_axis_column_name, residence_type):
+
+    dff = df_sanitation[df_sanitation['Country'].isin(x_axis_column_name)]
+    dff = dff.sort_values(by='Year')
+
+    fig = px.line(dff[dff['Residence Area Type'] == residence_type], x='Year', y='Display Value', markers=True,
+                      color='Country', title="Line Chart Showing Population using safely " + "<br>" +
+                                             "safely managed sanitation services (%)",
+                  color_discrete_map=c)
+    fig.update_layout(transition={'duration': 100,
+                                  'easing': 'linear'})
+
+    return fig
+
+# Bar chart for handwashing services
+@app.callback(
+    Output("stack_bar_handwashing", "figure"),
+    Input("country_dropdown", "value"),
+    Input("residence_dropdown", "value")
+)
+def update_stack_handwashing(x_axis_column_name, residence_type):
+    dff = df_handwashing[df_handwashing['Country'].isin(x_axis_column_name)]
+
+    fig = px.histogram(dff[dff['Residence Area Type'] == residence_type], x='Country', y='Display Value',
+                       histfunc='avg',
+                       color='Country', title="Bar Chart Showing Average Percentage (%) of Population using " + "<br>" +
+                                              "basic handwashing facilities at home",
+                       color_discrete_map=c)
+    fig.update_layout(transition={'duration': 100,
+                                  'easing': 'linear'})
+
+    return fig
+
+# Line chart for handwashing
+@app.callback(
+    Output("line_graph_handwashing", "figure"),
+    Input("country_dropdown", "value"),
+    Input("residence_dropdown", "value")
+)
+def update_line_handwashing(x_axis_column_name, residence_type):
+
+    dff = df_handwashing[df_handwashing['Country'].isin(x_axis_column_name)]
+    dff = dff.sort_values(by='Year')
+
+    fig = px.line(dff[dff['Residence Area Type'] == residence_type], x='Year', y='Display Value', markers=True,
+                      color='Country', title="Line Chart Showing Population using " + "<br>" +
+                                             "basic handwashing facilities at home (%)",
+                  color_discrete_map=c)
+    fig.update_layout(transition={'duration': 100,
+                                  'easing': 'linear'})
+
+    return fig
+
+# Bar chart for open defecation
+@app.callback(
+    Output("stack_bar_defecation", "figure"),
+    Input("country_dropdown", "value"),
+    Input("residence_dropdown", "value")
+)
+def update_stack_defecation(x_axis_column_name, residence_type):
+    dff = df_defecation[df_defecation['Country'].isin(x_axis_column_name)]
+
+    fig = px.histogram(dff[dff['Residence Area Type'] == residence_type], x='Country', y='Display Value',
+                       histfunc='avg',
+                       color='Country', title="Bar Chart showing average percentage of " + "<br>" +
+                                              "population practising open defecation (% Average)",
+                       color_discrete_map=c)
+    fig.update_layout(transition={'duration': 100,
+                                  'easing': 'linear'})
+
+    return fig
+
+# Line chart for open defecation
+@app.callback(
+    Output("line_graph_defecation", "figure"),
+    Input("country_dropdown", "value"),
+    Input("residence_dropdown", "value")
+)
+def update_line_defecation(x_axis_column_name, residence_type):
+
+    dff = df_defecation[df_defecation['Country'].isin(x_axis_column_name)]
+    dff = dff.sort_values(by='Year')
+
+    fig = px.line(dff[dff['Residence Area Type'] == residence_type], x='Year', y='Display Value', markers=True,
+                      color='Country', title="Line Chart Showing Population practising open defecation (%)",
+                  color_discrete_map=c)
+    fig.update_layout(transition={'duration': 100,
+                                  'easing': 'linear'})
 
     return fig
 
