bb51bbc16a7e79e2fa9e462acfdabf7602739d97
Samuel Gachuhi <sammigachuhi@gmail.com>
2023-07-10 16:03:17 +0300

    World health and sanitation app

--- sanitation_map.py
+++ sanitation_map.py
@@ -34,6 +34,7 @@ for dataframe in dataframe_table["Dataframes"]:
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
 
 # Layout for our data visualization application
 app.layout = html.Div(children=[
